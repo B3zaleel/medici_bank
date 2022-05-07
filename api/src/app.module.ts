8042ea9874430a1fabb09abcbc9ebf1329488caa
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-07 19:01:43 +0100

    Add GraphQL module to list of imports

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql', './**/*.gql'],
+      definitions: {
+        path: join(process.cwd(), 'src/types/graphql.ts'),
+        outputAs: 'class',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
