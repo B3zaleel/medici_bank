3410089b3202ee091573b381f67ad6603b8ed0e0
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-09 17:42:21 +0100

    Add User module

---
+++ api/src/modules/user.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserService } from 'src/services/user.service';
+import { User, UserSchema } from 'src/schemas/user.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [UserService],
+})
+export class UserModule {}
