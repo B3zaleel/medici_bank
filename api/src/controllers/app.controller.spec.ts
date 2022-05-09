d406ba38007f195ec0a8c602fb5fc5c9fd4ecf6e
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-09 17:40:33 +0100

    Update AppController's test module

--- api/src/controllers/app.controller.spec.ts
+++ api/src/controllers/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService } from '../app.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return OK status', () => {
+      expect(appController.getStatus()).toStrictEqual({ status: 'OK' });
     });
   });
 });
