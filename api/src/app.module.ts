4d4d25b27ce7ea6e2d5ae1f78d48134c0ff33c58
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-09 15:16:37 +0100

    Add authentication token extraction from the GraphQL context

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,6 +4,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './app.service';
 import { join } from 'path';
+import { DecodeAuthToken } from './utils/auth_helper';
 
 @Module({
   imports: [
@@ -13,6 +14,29 @@ import { join } from 'path';
       definitions: {
         path: join(process.cwd(), 'src/types/graphql.ts'),
         outputAs: 'class',
+      },
+      context: ({ req }) => {
+        const authTokenParts = (req.headers.authorization || '')
+          .trim()
+          .split(' ');
+        if (authTokenParts.length() == 2 && authTokenParts[0] == 'Bearer') {
+          const token = DecodeAuthToken(authTokenParts[1]);
+          if (!token) {
+            return {
+              userId: '',
+              isExpired: true,
+              errorMessage: 'Invalid authentication token',
+            };
+          }
+          const isExpired = token.exp <= Date.now();
+          const userId = isExpired ? null : token.userId;
+          return { userId, isExpired, errorMessage: '' };
+        }
+        return {
+          userId: '',
+          isExpired: true,
+          errorMessage: 'Missing authentication token',
+        };
       },
     }),
   ],
