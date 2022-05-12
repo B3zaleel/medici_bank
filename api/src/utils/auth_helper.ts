d031b6fd6eb1a46e41bc9f0d653b070f19b154fa
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-12 16:19:55 +0100

    Refactor module

--- api/src/utils/auth_helper.ts
+++ api/src/utils/auth_helper.ts
@@ -1,4 +1,5 @@
-import jwt from 'jsonwebtoken';
+import { sign, decode } from 'jsonwebtoken';
+import 'dotenv/config';
 
 /**
  * Represents the claims of an authentication token.
@@ -13,12 +14,12 @@ export interface Payload {
  * Decodes an authentication token.
  */
 export function DecodeAuthToken(token: string): Payload {
-  return jwt.decode(token) as Payload;
+  return decode(token) as Payload;
 }
 
 /**
  * Encodes claims for user authentication.
  */
 export function EncodeAuthClaims(userId: string): string {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
+  return sign({ userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
 }
