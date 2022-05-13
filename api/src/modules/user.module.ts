4c4e812f593d66f5cb1e85110b114887abf6c5c7
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-13 21:09:56 +0100

    Add user resolver

--- api/src/modules/user.module.ts
+++ api/src/modules/user.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserService } from 'src/services/user.service';
 import { User, UserSchema } from 'src/schemas/user.schema';
+import { UserResolver } from 'src/resolvers/user.resolver';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  providers: [UserService],
+  providers: [UserService, UserResolver],
   exports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
