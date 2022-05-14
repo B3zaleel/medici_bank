cfd56d5934758aff6664f18a77ae972a8385c888
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-14 12:10:49 +0100

    Update E2E controller tests

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -19,6 +19,10 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('{"status":"OK"}');
+  });
+
+  afterAll(() => {
+    app.close();
   });
 });
