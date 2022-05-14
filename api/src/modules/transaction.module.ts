b026a197c024444a48882709019349ef1d0096fd
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-14 04:08:23 +0100

    Add transaction module

---
+++ api/src/modules/transaction.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  TransactionResolver,
+  TransactionResultResolver,
+} from 'src/resolvers/transaction.resolver';
+import { TransactionService } from 'src/services/transaction.service';
+import { Transaction, TransactionSchema } from 'src/schemas/transaction.schema';
+import { UserModule } from './user.module';
+import { UserService } from 'src/services/user.service';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Transaction.name,
+        schema: TransactionSchema,
+      },
+    ]),
+    UserModule,
+  ],
+  providers: [
+    UserService,
+    TransactionService,
+    TransactionResolver,
+    TransactionResultResolver,
+  ],
+  exports: [
+    MongooseModule.forFeature([
+      {
+        name: Transaction.name,
+        schema: TransactionSchema,
+      },
+    ]),
+  ],
+})
+export class TransactionModule {}
