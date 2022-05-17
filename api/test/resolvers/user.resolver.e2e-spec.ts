34edebf002e578c8a178d4ef6f051a365a39a582
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-17 05:47:50 +0100

    Add tests for the user resolver's token expiration check

--- api/test/resolvers/user.resolver.e2e-spec.ts
+++ api/test/resolvers/user.resolver.e2e-spec.ts
@@ -2,8 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import * as mongoose from 'mongoose';
+import 'dotenv/config';
 import { AppModule } from '../../src/app.module';
 import { User, UserSchema } from '../../src/schemas/user.schema';
+import { sign } from 'jsonwebtoken';
 
 describe('UserResolver (e2e)', () => {
   let app: INestApplication;
@@ -31,6 +33,12 @@ describe('UserResolver (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
   });
+
+  function signExpiredToken(userId: string) {
+    const iat = Date.now();
+    const exp = iat - 1000 * 60 * 60 * 24 * 30;
+    return sign({ userId, iat, exp }, process.env.JWT_SECRET);
+  }
 
   it('Unauthorized user fetch fails', async () => {
     const query = `
@@ -150,6 +158,39 @@ describe('UserResolver (e2e)', () => {
     expect(body['getUser']['name']).toEqual(sampleUser.name);
     expect(body['getUser']['phone']).toEqual(sampleUser.phone);
     expect(body['getUser']['balance']).toEqual(0);
+  });
+
+  it('User fetch with an expired token fails', async () => {
+    const UserModel = mongoose.model(User.name, UserSchema);
+    const user = await UserModel.findOne({ phone: sampleUser.phone }).exec();
+    const oldToken = signExpiredToken(user.id);
+    const query = `
+      query {
+        getUser {
+          __typename
+          ... on User {
+            name
+            phone
+            balance
+          }
+          ... on ActionResult {
+            success
+            message
+          }
+        }
+      }
+    `;
+
+    const res = await request(app.getHttpServer())
+      .post('/graphql')
+      .set('Authorization', `Bearer ${oldToken}`)
+      .send({ query })
+      .expect(200);
+    const body = JSON.parse(res.text).data;
+    expect(body).toBeDefined();
+    expect(body['getUser']['__typename']).toEqual('ActionResult');
+    expect(body['getUser']['success']).toEqual(false);
+    expect(body['getUser']['message']).toEqual('Token has expired');
   });
 
   it('Authorized user account deletion with invalid phone fails', async () => {
