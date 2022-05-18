7c0a7b3daa5dc99198c7119b9f0ee5ac4b467138
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-18 08:43:49 +0100

    Add route definitions to router

--- web_client/src/router/index.ts
+++ web_client/src/router/index.ts
@@ -1,26 +1,101 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: '/',
+    name: 'home',
+    meta: {
+      title: 'Home',
+      requiresAuth: false,
+    },
+    component: () => import('../views/Home.vue'),
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: '/sign-in',
+    name: 'sign_in',
+    meta: {
+      title: 'Sign In',
+      requiresAuth: false,
+      disableOnSignedIn: true,
+    },
+    component: () => import('../views/auth/SignIn.vue'),
+  },
+  {
+    path: '/sign-up',
+    name: 'sign_up',
+    meta: {
+      title: 'Sign Up',
+      requiresAuth: false,
+      disableOnSignedIn: true,
+    },
+    component: () => import('../views/auth/SignUp.vue'),
+  },
+  {
+    path: '/delete-account',
+    name: 'delete_account',
+    meta: {
+      title: 'Delete Account',
+      requiresAuth: true,
+    },
+    component: () => import('../views/auth/DeleteAccount.vue'),
+  },
+  {
+    path: '/services',
+    name: 'services',
+    meta: {
+      title: 'Services',
+      requiresAuth: true,
+    },
+    component: () => import('../views/Services.vue'),
+  },
+  {
+    path: '/transactions',
+    name: 'transactions',
+    meta: {
+      title: 'Transactions',
+      requiresAuth: true,
+    },
+    component: () => import('../views/Transactions.vue'),
+  },
+  {
+    path: '/about',
+    name: 'about',
+    meta: {
+      title: 'Home',
+      requiresAuth: false,
+    },
+    component: () => import('../views/AboutView.vue'),
+  },
+  {
+    path: '*',
+    name: 'error404',
+    meta: {
+      title: 'Error 404',
+      requiresAuth: false,
+    },
+    component: () => import('../views/ErrorView.vue'),
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+// router.beforeEach((to, _, next) => {
+//   const isAuthenticated =
+//     window.localStorage.getItem('user.isAuthenticated') === 'true';
+//   if (!isAuthenticated && to.meta?.requiresAuth) {
+//     next('/sign-in');
+//   } else if (isAuthenticated && to.meta?.disableOnSignedIn) {
+//     next('/');
+//   } else {
+//     next();
+//   }
+// });
+
+router.afterEach((to) => {
+  document.title = to.meta?.title ? `${to.meta.title} - Medici` : 'Medici';
 });
 
 export default router;
