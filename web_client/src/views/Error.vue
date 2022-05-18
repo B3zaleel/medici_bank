432a47f5ccf5973716d05a3e9275fc68ec7f21db
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-18 16:24:48 +0100

    Add Error404 view

---
+++ web_client/src/views/Error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Page Not Found</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class Error extends Vue {}
+</script>
+
+<style lang="sass">
+/*  */
+</style>
