e7c6d93d1fd1b5896e0eb1fef492a10bc77d9cd0
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-18 17:13:20 +0100

    Update Home view

--- web_client/src/views/Home.vue
+++ web_client/src/views/Home.vue
@@ -1,30 +1,72 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    <div></div>
+    <div :class="{ 'guest-panel': true, visible: !isAuthenticated() }">
+      <h2>Welcome to Medici Bank</h2>
+      <div class="cta-panel">
+        <strong>Want to join us?</strong>
+        <router-link to="/sign-up" class="mb-button-rounded"
+          >Sign Up
+        </router-link>
+        <strong>Already have an account?</strong>
+        <router-link to="/sign-in" class="mb-button-rounded"
+          >Sign In
+        </router-link>
+      </div>
+    </div>
+
+    <div :class="{ 'user-panel': true, visible: isAuthenticated() }">
+      <div class="profile-info">
+        <AccountCircle />
+        <strong class="name">{{ name }}</strong>
+        <strong class="balance">&#x20A6; {{ balance / 100 }}</strong>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import { useStore } from 'vuex';
-import HelloWorld from '@/components/HelloWorld.vue';
+import AccountCircle from '@/assets/icons/AccountCircle.vue';
+import Naira from '@/assets/icons/Naira.vue';
+import UserAPIReq from '@/modules/api_requests/user';
 
 @Options({
   components: {
-    HelloWorld,
+    AccountCircle,
+    Naira,
   },
 })
-export default class HomeView extends Vue {
-  items = [
-    {
-      label: 'Services',
-      icon: '',
-    },
-  ];
+export default class Home extends Vue {
+  store = useStore();
+  userAPIReq = new UserAPIReq(
+    this.store.state.API_URL,
+    this.store.state.user.authToken,
+  );
+  name = 'Bezaleel Olakunori';
+  balance = 254500;
+
+  isAuthenticated() {
+    return this.store.state.user.isAuthenticated;
+  }
+
+  loadUserInfo(): void {
+    this.userAPIReq.getUser().then((res) => {
+      if (res.__typename === 'User') {
+        this.name = res.name;
+        this.balance = res.balance;
+      }
+    });
+  }
+
+  mounted() {
+    if (this.isAuthenticated()) {
+      this.loadUserInfo();
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-//
+@use '@/assets/styles/home';
 </style>
