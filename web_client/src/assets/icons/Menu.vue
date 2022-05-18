9c3b7ee54630904d011eddb1b0df7e34ab9bb6a0
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-18 09:28:23 +0100

    Add icons

---
+++ web_client/src/assets/icons/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    version="1.1"
+    id="mdi-menu"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+  </svg>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class Menu extends Vue {}
+</script>
+
+<style></style>
