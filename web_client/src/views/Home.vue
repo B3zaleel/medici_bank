b1768956b575de74f591ab71ed32266efb675396
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-18 08:42:20 +0100

    Rename HomeView.vue to Home.vue

---
+++ web_client/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import { useStore } from 'vuex';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class HomeView extends Vue {
+  items = [
+    {
+      label: 'Services',
+      icon: '',
+    },
+  ];
+}
+</script>
+
+<style lang="scss">
+//
+</style>
